379e145003af43439bc3d57ff7f233864af4f414
FmsJoao <joao.victorfmaciel@gmail.com>
2022-02-19 17:06:27 -0300

    Add a test case for the problem of cyclic rotation

--- tests_codility_challenges.py
+++ tests_codility_challenges.py
@@ -16,3 +16,12 @@ binary = [(1041, 5),
 @pytest.mark.parametrize(['N', 'expected'], binary)
 def test_binary_gap(N, expected):
     assert codility_challenges.binary_gap(N) == expected
+
+cyclic = [([1, 2, 3, 4, 5], 4, [2, 3, 4, 5, 1]),
+            ([0, 3, 5, 9], 3, [3, 5, 9, 0]),
+            ([0, 0, 1], 3, [0, 0, 1]),
+            ([5, 8, 7, 2, 4, 6, 9, 5], 6, [7, 2, 4, 6, 9, 5, 5, 8])]
+
+@pytest.mark.parametrize(['A', 'K', 'expected'], cyclic)
+def test_cyclic_rotation(A, K, expected):
+    assert codility_challenges.cyclic_rotation(A, K) == expected
